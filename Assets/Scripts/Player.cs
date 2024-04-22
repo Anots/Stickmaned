9eb59a72abd0771f0d6ff7e2ecc22d1cbe8674dc
Neroxgd <darkaux69@gmail.com>
2024-04-22 21:29:44 +0200

    visual studio

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 direction;
+    private Rigidbody2D rb2D;
+    private void Movement(InputAction.CallbackContext context)
     {
-        
+        direction = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rb2D.velocity = direction;
     }
 }
